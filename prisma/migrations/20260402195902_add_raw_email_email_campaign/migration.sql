f1ad51aef4047462ac5ac1ae87dc27478af6e448
Drew Williams <drew.williams@uplifterinc.com>
2026-04-02 16:17:11 -0400

    fix: correct raw email campaign migration to no-op (rawBody already added in 20260401195757)

--- prisma/migrations/20260402195902_add_raw_email_email_campaign/migration.sql
+++ prisma/migrations/20260402195902_add_raw_email_email_campaign/migration.sql
@@ -1,15 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `termsAcceptedAt` on the `User` table. All the data in the column will be lost.
-  - You are about to drop the `CacheVersion` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Organization" ADD COLUMN     "processingFeePaidBy" "FeePayer" NOT NULL DEFAULT 'CUSTOMER';
-
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "termsAcceptedAt";
-
--- DropTable
-DROP TABLE "CacheVersion";
+-- rawBody already added in 20260401195757_add_raw_body_to_email_campaign
+SELECT 1;
