64c3a7a7542a3552acea3c7bf005cd4508b2c2ae
drewwilliams1995 <drew.williams@uplifterinc.com>
2026-04-14 13:04:39 -0400

    fix: USC-313 fix: udpate sentry to include console error logs (#342)

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,4 +9,6 @@ Sentry.init({
   tracesSampleRate: process.env.APP_ENVIRONMENT === "production" ? 0.1 : 1.0,
 
   environment: process.env.APP_ENVIRONMENT || process.env.NODE_ENV,
+
+  integrations: [Sentry.captureConsoleIntegration({ levels: ["error"] })],
 });
